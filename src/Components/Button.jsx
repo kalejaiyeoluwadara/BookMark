40a11d282abf32fc6f6c0970f226d29b76625e17
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-24 23:05:36 +0100

    home page

---
+++ src/Components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button({ name }) {
+  return (
+    <button className="w-[160px] opacity-[0.7] h-[50px] border-2 border-gray-800 rounded-[3px] flex items-center justify-center text-[16px] font-[600] ">
+      {name}
+    </button>
+  );
+}
+
+export default Button;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-
+import illustration from "../assets/images/illustration-hero.svg";
+import dot from "../assets/images/bg-dots.svg";
+import Button from "../Components/Button";
 function Home() {
   return (
-    <div>
-      <div>
-        <h1></h1>
-        <p></p>
-        <section>
-          <button></button>
-          <button></button>
+    <div className="w-screen h-[100vh] bgi flex py-20 px-10 items-center justify-center">
+      {/* Text content Div */}
+      {/* <img className="absolute right-10 top-10" src={dot} alt="" /> */}
+      <div className="sm:w-[40%] flex flex-col ">
+        <h1 className="font-[600] opacity-[0.9] w-[400px] text-[40px] ">
+          A Simple Bookmark Manager
+        </h1>
+        <p className="w-[380px] mt-2 tex-[22px] font-[500] opacity-[0.8]">
+          A clean and simple interface to organize your favourite websites. Open
+          a new browser tab and see your sites load instantly. Try it for free.
+        </p>
+        <section className="mt-4 flex gap-4 w-full ">
+          <Button name={"Get it on Chrome"} />
+          <Button name={"Get it on Firefox"} />
         </section>
       </div>
       {/* Illustration Div */}
-      <div>
-        <img src="" alt="" />
+      <div className="sm:w-[40%] flex items-center justify-center ">
+        <img src={illustration} alt="" />
       </div>
     </div>
   );
