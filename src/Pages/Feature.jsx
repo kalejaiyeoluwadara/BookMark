227a61cca2531e1b361948b469d4399cd43946a1
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-25 10:58:49 +0100

    update

--- src/Pages/Feature.jsx
+++ src/Pages/Feature.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-
+import Button from "../Components/Button";
+import illus from "../assets/images/illustration-features-tab-1.svg";
+import illus1 from "../assets/images/illustration-features-tab-2.svg";
+import illus2 from "../assets/images/illustration-features-tab-3.svg";
 function Feature() {
   const [countstate, setCounstate] = useState(0);
   return (
@@ -22,14 +25,20 @@ function Feature() {
             {
               name: "Simple BookMarking",
               count: 0,
+              img: illus,
+              content: "",
             },
             {
               name: "Speedy Searching",
               count: 1,
+              img: illus,
+              content: "",
             },
             {
               name: "Easy Sharing",
               count: 2,
+              img: illus,
+              content: "",
             },
           ].map((nav, id) => {
             const { name, count } = nav;
@@ -43,6 +52,22 @@ function Feature() {
             );
           })}
         </nav>
+
+        {/* Dynamic Content */}
+        <div className="flex w-full gap-10 items-center justify-center">
+          <section>
+            <img className="sm:w-[40%]" src={illus} alt="" />
+          </section>
+          <section className="flex flex-col gap-3 items-center justify-center">
+            <h3>Bookmark in one click</h3>
+            <p>
+              Organize your bookmarks however you like. Our simple drag-and-drop
+              interface gives you complete control over how you manage your
+              favourite sites.
+            </p>
+            <Button name={"More info"} blue={true} />
+          </section>
+        </div>
       </section>
     </div>
   );
@@ -53,7 +78,7 @@ const Link = ({ name, count, countstate, setCounstate }) => {
       onClick={() => {
         setCounstate(count);
       }}
-      className={` py-3 px-8 rounded-[4px] cursor-pointer text-[20px] ${
+      className={` py-5 px-8 rounded-[4px] cursor-pointer text-[20px] ${
         count == countstate ? "border-b-[5px] font-[600] border-orange-600" : ""
       } `}
     >
