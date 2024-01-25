b3d0db6f7ce7e013ce8ba5d3d131e815ecb7fe4c
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-25 08:45:52 +0100

    Nav Done

--- src/Pages/Feature.jsx
+++ src/Pages/Feature.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Feature() {
+  const [countstate, setCounstate] = useState(0);
   return (
     <div className="h-[90vh] w-screen flex flex-col items-center justify-center">
       {/* First Section */}
@@ -16,30 +17,48 @@ function Feature() {
       {/* Second Section */}
       <section className="bg-white w-full flex items-center justify-center flex-col">
         {/* Nav section */}
-        <nav className="flex h-[15vh] border-b-[1px] mt-12 sm:w-[70%] items-center justify-center border-opacity-[0.3] ">
+        <nav className="flex h-[15vh] border-b-[1px] mt-12 sm:w-auto gap-10 items-end justify-center border-opacity-[0.3] ">
           {[
             {
               name: "Simple BookMarking",
               count: 0,
             },
             {
-              name: "Simple BookMarking",
-              count: 0,
+              name: "Speedy Searching",
+              count: 1,
             },
             {
-              name: "Simple BookMarking",
-              count: 0,
+              name: "Easy Sharing",
+              count: 2,
             },
           ].map((nav, id) => {
             const { name, count } = nav;
-            return <Link name={name} count={count} />;
+            return (
+              <Link
+                countstate={countstate}
+                setCounstate={setCounstate}
+                name={name}
+                count={count}
+              />
+            );
           })}
         </nav>
       </section>
     </div>
   );
 }
-const Link = ({ name, count }) => {
-  return <p>{name}</p>;
+const Link = ({ name, count, countstate, setCounstate }) => {
+  return (
+    <p
+      onClick={() => {
+        setCounstate(count);
+      }}
+      className={` py-3 px-8 rounded-[4px] cursor-pointer text-[20px] ${
+        count == countstate ? "border-b-[5px] font-[600] border-orange-600" : ""
+      } `}
+    >
+      {name}
+    </p>
+  );
 };
 export default Feature;
