a9621a0d52af194ca4acca86d052783b140efa8b
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-25 15:36:21 +0100

    updates

--- src/Components/Quest.jsx
+++ src/Components/Quest.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import icon from "../assets/images/icon-arrow.svg";
-function Quest() {
+function Quest({ question, answer }) {
+  const [isOPened, setIsOpened] = useState(false);
   return (
-    <div className="">
-      <section>
-        <p>What is Bookmark ?</p>
+    <div className=" sm:w-[800px] flex flex-col ">
+      <section className=" py-6 w-full border-t-2 border-opacity-25 border-gray-400 flex  justify-between items-center">
+        <p className="text-[20px]  ">{question}</p>
+        <img
+          src={icon}
+          className={`cursor-pointer hover:text-orange-600 ${
+            isOPened ? "rotate-180 " : " "
+          } `}
+          onClick={() => {
+            setIsOpened(!isOPened);
+          }}
+          alt=""
+        />
       </section>
-      <section></section>
+      {/* Content */}
+      {isOPened && (
+        <section>
+          <p className="text-[20px] mb-6 w-[100%] opacity-80 ">{answer}</p>
+        </section>
+      )}
     </div>
   );
 }
--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Components/Header";
+import Quest from "../Components/Quest";
+import Button from "../Components/Button";
 
 function Faq() {
+  const [faqs, setFaqs] = useState([
+    {
+      question: "What is Bookmark?",
+      answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt 
+  justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.`,
+    },
+    {
+      question: "How can I request a new browser?",
+      answer: `Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. 
+  Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, 
+  ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. 
+  Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.
+`,
+    },
+    {
+      question: "Is there a mobile app?",
+      answer: `Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum 
+  urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed 
+  sollicitudin ex et ultricies bibendum.`,
+    },
+    {
+      question: "What about other Chromium browsers?",
+      answer: ` Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam 
+  vitae neque eget nisl gravida pellentesque non ut velit.`,
+    },
+  ]);
   return (
-    <div className="h-[100vh] w-screen flex items-center justify-center ">
+    <div className="h-[100vh] w-screen flex flex-col items-center justify-center ">
       <Header
         name={"Frequently Asked Questions"}
         content={`Here are some of our FAQs. If you have any other questions you’d like 
   answered please feel free to email us.`}
       />
+
+      {/* Faqs */}
+      <section className="mt-10 flex flex-col ">
+        {faqs.map((item, id) => {
+          const { question, answer } = item;
+          return <Quest key={id} question={question} answer={answer} />;
+        })}
+      </section>
+      <section className="flex w-full items-center mt-8 justify-center">
+        <Button name={"More Info"} blue={true} />
+      </section>
     </div>
   );
 }
