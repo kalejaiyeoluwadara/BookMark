16dab1f915b401dabc7c480dd1cfd44d839f8c48
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-26 21:50:59 +0100

    resp

--- src/Pages/Nav.jsx
+++ src/Pages/Nav.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "../Components/Link";
 import logo from "../assets/images/logo-bookmark.svg";
+import logo from "../assets/images/logo-bookmark.svg";
 function Nav() {
   return (
-    <div className="fixed z-40 px-20 h-[15vh] top-0 left-0 w-screen bg-white flex items-center justify-between">
+    <div className="fixed z-40 sm:px-20 px-12 sm:h-[15vh] h-[12vh] top-0 left-0 w-screen bg-white flex items-center justify-between">
       <div>
         <img src={logo} alt="" />
       </div>
-      <ul className="flex justify-between items-center gap-10  ">
+      <ul className="sm:flex sm justify-between items-center gap-10  ">
         {[
           {
             name: "Features",
@@ -38,6 +39,9 @@ function Nav() {
           );
         })}
       </ul>
+      <div>
+        <img src={logo} alt="" />
+      </div>
     </div>
   );
 }
