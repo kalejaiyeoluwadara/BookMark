860deee1d2346440bb524a6ec7b40b057a138e2e
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-30 21:22:25 +0100

    menu page

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <div className="fixed h-screen w-screen flex items-center justify-center ">
+      Menu
+    </div>
+  );
+}
+
+export default Menu;
