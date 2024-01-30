f55ce8bb042d311dae50a1df6b0d7cd531ad0788
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-30 21:46:20 +0100

    menu page

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "../assets/images/logo-bookmark.svg";
-import logo from "../assets/images/logo-bookmark.svg";
+import cancel from "../assets/images/icon-close.svg";
 function Menu() {
   return (
-    <div className="fixed top-0 left-0 bgg z-50 h-screen w-screen flex items-center justify-center ">
-      <div>
+    <div className="fixed top-0 left-0 bgg z-50 h-screen w-screen flex items-start justify-center ">
+      <div className="flex justify-between items-center w-full px-12 pt-12 ">
         <img src={logo} alt="logo" />
+        <img src={cancel} alt="cancel" />
       </div>
     </div>
   );
--- src/Pages/Nav.jsx
+++ src/Pages/Nav.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/images/logo-bookmark.svg";
 import nav from "../assets/images/icon-hamburger.svg";
 function Nav() {
   return (
-    <div className="fixed z-40 sm:px-20 px-12 sm:h-[15vh] h-[12vh] top-0 left-0 w-screen bg-white flex items-center justify-between">
+    <div className="absolute z-40 sm:px-20 px-12 sm:h-[15vh] h-[12vh] top-0 left-0 w-screen bg-white flex items-center justify-between">
       <div>
         <img src={logo} alt="" />
       </div>
