bbbb4fb096b8956aa6c1c788b58a21d96e546798
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-02-01 18:28:36 +0100

    menu page

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/logo-bookmark.svg";
+import face from "../assets/images/icon-facebook.svg";
+import twiter from "../assets/images/icon-twitter.svg";
 import cancel from "../assets/images/icon-close.svg";
 function Menu() {
+  const [links, setLinks] = useState([
+    {
+      name: "Features",
+      special: false,
+      link: "#features",
+    },
+    {
+      name: "Pricing",
+      special: false,
+      link: "#pricing",
+    },
+    {
+      name: "Contact",
+      special: false,
+      link: "#contact",
+    },
+    {
+      name: "Login",
+      special: true,
+      link: "#contact",
+    },
+  ]);
   return (
-    <div className="fixed top-0 left-0 bgg z-50 h-screen w-screen flex items-start justify-center ">
+    <div className="fixed top-0 left-0 bgg z-50 h-screen w-screen flex flex-col items-center justify-start ">
       <div className="flex justify-between items-center w-full px-12 pt-12 ">
         <img src={logo} alt="logo" />
         <img src={cancel} alt="cancel" />
       </div>
       {/* Links */}
-      <div>{}</div>
+      <div className="flex text-white text-[24px] font-[500] mt-10 gap-10 items-center justify-center flex-col w-screen ">
+        {links.map((item, id) => {
+          const { name, speacial, link } = item;
+          return (
+            <a className=" border-y border-white  " href={link}>
+              {name}
+            </a>
+          );
+        })}
+      </div>
       {/* Footer */}
+      <div className="absolute bottom-10 w-screen flex items-center justify-center gap-20">
+        {[face, twiter].map((log, id) => {
+          return <img src={log} key={id} alt="" />;
+        })}
+      </div>
     </div>
   );
 }
--- src/Pages/Nav.jsx
+++ src/Pages/Nav.jsx
@@ -1,37 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import Link from "../Components/Link";
 import logo from "../assets/images/logo-bookmark.svg";
 import nav from "../assets/images/icon-hamburger.svg";
 function Nav() {
-  const { links, setLinks } = useState([
-    {
-      name: "Features",
-      special: false,
-      link: "#features",
-    },
-    {
-      name: "Pricing",
-      special: false,
-      link: "#pricing",
-    },
-    {
-      name: "Contact",
-      special: false,
-      link: "#contact",
-    },
-    {
-      name: "Login",
-      special: true,
-      link: "#contact",
-    },
-  ]);
   return (
     <div className="absolute z-40 sm:px-20 px-12 sm:h-[15vh] h-[12vh] top-0 left-0 w-screen bg-white flex items-center justify-between">
       <div>
         <img src={logo} alt="" />
       </div>
       <ul className="sm:flex sm justify-between items-center gap-10  ">
-        {links.map((item, id) => {
+        {[
+          {
+            name: "Features",
+            special: false,
+            link: "#features",
+          },
+          {
+            name: "Pricing",
+            special: false,
+            link: "#pricing",
+          },
+          {
+            name: "Contact",
+            special: false,
+            link: "#contact",
+          },
+          {
+            name: "Login",
+            special: true,
+            link: "#contact",
+          },
+        ].map((item, id) => {
           const { name, special, link } = item;
           return (
             <li>
