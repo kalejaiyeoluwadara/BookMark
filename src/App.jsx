3dfcedadebd2399770a7d92332cb1c53bd08bdfe
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-02-02 20:55:21 +0100

    removed stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,7 @@ const App = () => {
     <>
       <AnimatePresence>{menu && <Menu />}</AnimatePresence>
       <Nav />
-      <Reveal>
-        <Home />
-      </Reveal>
+      <Home />
       <Reveal>
         <Feature />
       </Reveal>
